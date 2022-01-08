23ef5fc8ec297b698430e52dcf2213e8e1e240ca
Alex <altoullec@yahoo.fraltoullec@yahoo.frgit config --global user.email altoullec@yahoo.fr>
2022-01-08 15:53:39 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const inpCouleur = document.querySelectorAll('.inp-Couleur');
+const inpRange = document.querySelector('.inp-range');
+
+
+
+// initialisation
+
+let valCouleur = ['#BA5370', '#F4ED28'];
+let inclinaison = 45;
+
