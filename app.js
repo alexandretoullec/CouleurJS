90c6b80a518f4e6c776fbf4a08cda9fe575607e9
Alex <altoullec@yahoo.fraltoullec@yahoo.frgit config --global user.email altoullec@yahoo.fr>
2022-01-08 19:03:28 +0100

    third commit

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 const inpCouleur = document.querySelectorAll('.inp-Couleur');
 const inpRange = document.querySelector('.inp-range');
-
-
+const fond = document.body;
+const btns = document.querySelectorAll('button')
 
 // initialisation
 
-let valCouleur = ['#BA5370', '#F4ED28'];
+let valCouleur = ['#BA5370', '#F4E2D8'];
 let inclinaison = 45;
+let index = 3;
 
+inpCouleur[0].value = valCouleur[0];
+inpCouleur[1].value = valCouleur[1];
+inpCouleur[0].style.background = valCouleur[0];
+inpCouleur[1].style.background = valCouleur[1];
+
+fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur[0]}, ${valCouleur[1]})`
+
+//inclinaison
+
+inpRange.addEventListener('input', (e) => {
+    inclinaison = e.target.value * 3.6;
+    fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleur[0]}, ${valCouleur[1]})`;
+})
+
+// Rajout ou suppression couleur
+
+btns.forEach(btn => {
+
+    btn.addEventListener('click', rajouteEnleve)
+}
+)
+
+function rajouteEnleve(e) {
+    const allInput = document.querySelectorAll('.inp-Couleur');
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    if (e.target.className === "+") {
+        if (allInput.length > 8) {
+            return;
+        }
+
+        const nvCouleur = document.createElement('input');
+        nvCouleur.setAttribute('class', 'inp-Couleur');
+        nvCouleur.setAttribute('data-index', index);
+        nvCouleur.setAttribute('maxlength', 7);
+    }
+}
